d1a286202ccd99da89afc7d426b0904e76eb6377
DESKTOP-FRDIMOJ\abhin <asharm28@binghamton.edu>
2017-04-24 10:18:25 -0400

    title movement with click

--- project07/Project7/Assets/Scripts/TheStack.cs
+++ project07/Project7/Assets/Scripts/TheStack.cs
@@ -2,16 +2,25 @@
 using System.Collections;
 using UnityEngine;
 
-public class TheStack : MonoBehaviour {
+public class TheStack : MonoBehaviour
+{
+    private const float BOUND_SIZE = 3.5f;
+
     private GameObject[] theStack;
+
     private int stackIndex;
     private int scoreCount = 0;
 
-	// Use this for initialization
-	private void Start () {
+    private float tileTransition = 0.0f;
+    private float titleSpeed = 2.5f;
+
+    // Use this for initialization
+    private void Start()
+    {
         theStack = new GameObject[transform.childCount];
-        
-        for (int i = 0; i < transform.childCount; i++) {
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
             theStack[i] = transform.GetChild(i).gameObject;
         }
         stackIndex = transform.childCount - 1;
@@ -21,13 +30,42 @@ public class TheStack : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) { 
-            PlaceTitle();
-            scoreCount++;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (PlaceTitle())
+            {
+                SpawnTitle();
+                scoreCount++;
+            }
+            else {
+                EndGame();
+            }
         }
-	}
+        MoveTile();
+    }
 
-    private void PlaceTitle(){
+    private void MoveTile()
+    {
+        tileTransition += Time.deltaTime * titleSpeed;
+        theStack[stackIndex].transform.localPosition = new Vector3(Mathf.Sin(tileTransition * BOUND_SIZE), scoreCount, 0);
+
+    }
+
+    private void EndGame()
+    {
+
+    }
+
+    private void SpawnTitle()
+    {
+        stackIndex--;
+        if (stackIndex < 0)
+            stackIndex = transform.childCount - 1;
         theStack[stackIndex].transform.localPosition = new Vector3(0, scoreCount, 0);
+
+    }
+    private bool PlaceTitle()
+    {
+        return true;
     }
 }
