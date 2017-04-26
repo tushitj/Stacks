2de1bb4184303f0714c61f535c3ff8119a28455b
Tushit Jain <tjain3@binghamton.edu>
2017-04-25 20:03:41 -0400

    Last tile position updated. Game End logic

--- project07/Project7/Assets/Scripts/TheStack.cs
+++ project07/Project7/Assets/Scripts/TheStack.cs
@@ -6,10 +6,14 @@ public class TheStack : MonoBehaviour
 {
     private const float BOUND_SIZE = 3.5f;
 	private const float STACK_MOVING_SPEED = 5.0f;
+	private const float ERROR_MARGIN = 0.1f;
+
     private GameObject[] theStack;
+	private Vector2 stackBounds = new Vector2 (BOUND_SIZE, BOUND_SIZE);
 
     private int stackIndex;
     private int scoreCount = 0;
+	private int combo = 0;
 
     private float tileTransition = 0.0f;
     private float titleSpeed = 2.5f;
@@ -18,6 +22,8 @@ public class TheStack : MonoBehaviour
 	private bool isMovingOfX = true;
 
 	private Vector3 desiredPosition;
+	private Vector3 lastTilePosition;
+
 
     // Use this for initialization
     private void Start()
@@ -66,6 +72,7 @@ public class TheStack : MonoBehaviour
 
     private void SpawnTitle()
     {
+		lastTilePosition = theStack [stackIndex].transform.localPosition;
         stackIndex--;
         if (stackIndex < 0)
             stackIndex = transform.childCount - 1;
@@ -75,13 +82,22 @@ public class TheStack : MonoBehaviour
 
     }
     private bool PlaceTitle()
-    {
+	{
 		Transform t = theStack [stackIndex].transform;
 
 		if (isMovingOfX) {
-			//float delta = t.position.x;
-		}
+			float deltaX = lastTilePosition - t.position.x;
+			if (Mathf.Abs (deltaX) > ERROR_MARGIN) {
+				combo = 0;
+				stackBounds.x -= MAthf.Abs (deltaX);
+				if (stackBounds.x <= 0)
+					return false;
 
+				float middle = lastTilePosition.x + t.localPositon.x / 2;
+				t.localScale()
+
+			}
+		}
 		secondaryPosition = (isMovingOfX)
 			? t.localPosition.x
 			: t.localPosition.z;
