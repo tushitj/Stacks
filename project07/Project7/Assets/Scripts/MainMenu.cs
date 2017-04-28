288681464d9cee3551dce73c114da6d2d2508842
Tushit Jain <tjain3@binghamton.edu>
2017-04-28 19:16:30 -0400

    Unity file updates

---
+++ project07/Project7/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	public Text scoreText;
+
+	private void start(){
+		scoreText.text = PlayerPrefs.GetInt ("score").ToString ();
+	}
+		
+	public void ToGame(){
+		SceneManager.LoadScene ("Game");
+	}
+
+}
