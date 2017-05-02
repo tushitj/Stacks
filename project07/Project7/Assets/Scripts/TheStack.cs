b6ca86075a3830457239c4fe0ba23400490059f5
Tushit Jain <tjain3@binghamton.edu>
2017-05-01 21:43:09 -0400

    lerp function remove. not working correct

--- project07/Project7/Assets/Scripts/TheStack.cs
+++ project07/Project7/Assets/Scripts/TheStack.cs
@@ -195,15 +195,6 @@ public class TheStack : MonoBehaviour
 		mesh.colors32 = colors;
 	
 	 }
-	private Color32 Lerp4 (Color32 a, Color32 b, Color32 c, Color32 d,float t){
-		if (t < 0.33f)
-			return Color.Lerp (a, b, (t / 0.33f));
-		else if (t < 0.66f)
-			return Color.Lerp (b, c, (t - 0.33f) / 0.33f);
-		else
-			return Color.Lerp (c, d, (t - 0.66f) / 0.66f);
-	
-	}
 
 	private void EndGame (){
 		if (PlayerPrefs.GetInt ("score") < scoreCount)
